f20a29b0e467d1a7d08bd55c22c1247385b3562b
Asrul Firdaus <asrulfirdaus93@gmail.com>
2026-01-09 11:15:28 +0800

    added user handle to create new user using admin account

---
+++ src/schemas/user_schema.rs
@@ -0,0 +1,25 @@
+use serde::{
+    Serialize,
+    Deserialize
+};
+use chrono::{DateTime, Utc};
+use validator::Validate;
+use uuid::Uuid;
+
+#[derive(Deserialize, Validate)]
+pub struct UserNewRequest {
+    #[validate(length(min = 3, message = "Name cannot be less than 3 characters"))]
+    pub name: String,
+    #[validate(email(message = "Email is not valid"))]
+    pub email: String,
+    #[validate(length(min = 6, message = "Password must be 6 characters"))]
+    pub password: String,
+}
+
+pub struct UserNewResponse {
+    pub id: Uuid,
+    pub name: String,
+    pub email: String,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
